9720a372348a7318b581f2ccc2ffffcfb8ce1af9
Rizqdwan <rizqdwandhuhakbar@gmail.com>
2026-05-23 21:31:16 +0700

    feat: setup department and group service

--- internal/organization/department/model.go
+++ internal/organization/department/model.go
@@ -1,27 +1,43 @@
 package department
 
-import "time"
+import (
+	"time"
+
+	"github.com/rizqdwan/go-mono-project/internal/organization/group"
+)
 
 type Department struct {
 	ID         int64     `db:"id"`
 	Label      string    `db:"label"`
 	Name       string    `db:"name"`
-	group_id   int64     `db:"group_id"`
-	created_at time.Time `db:"created_at"`
-	updated_at time.Time `db:"updated_at"`
+	Group_id   int64     `db:"group_id"`
+	Created_at time.Time `db:"created_at"`
+	Updated_at time.Time `db:"updated_at"`
 }
 
 // @name DepartmentResponse
 type DepartmentResponse struct {
-	ID        int64  `json:"id"`
-	Label     string `json:"label"`
-	Name      string `json:"name"`
-	GroupID   int64  `json:"group_id"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	ID        int64           `json:"id"`
+	Label     string          `json:"label"`
+	Name      string          `json:"name"`
+	Group     group.GroupInfo `json:"group"`
+	CreatedAt time.Time       `json:"created_at"`
+	UpdatedAt time.Time       `json:"updated_at"`
 }
 
 type DepartmentInfo struct {
 	Label string `json:"label"`
 	Name  string `json:"name"`
+}
+
+type NewDepartmentRequest struct {
+	Label   string `json:"label" validate:"required"`
+	Name    string `json:"name" validate:"required"`
+	GroupID int64  `json:"group_id" validate:"required"`
+}
+
+type UpdateDepartmentRequest struct {
+	Label   string `json:"label" validate:"required"`
+	Name    string `json:"name" validate:"required"`
+	GroupID int64  `json:"group_id" validate:"required"`
 }
--- internal/organization/group/model.go
+++ internal/organization/group/model.go
@@ -1,17 +1,34 @@
 package group
 
+import "time"
+
 type Group struct {
-	ID        int64  `db:"id"`
-	Label     string `db:"label"`
-	Name      string `db:"name"`
-	CreatedAt string `db:"created_at"`
-	UpdatedAt string `db:"updated_at"`
+	ID        int64     `db:"id"`
+	Label     string    `db:"label"`
+	Name      string    `db:"name"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
 }
 
 type GroupResponse struct {
-	ID        int64  `json:"id"`
-	Label     string `json:"label"`
-	Name      string `json:"name"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	ID        int64     `json:"id"`
+	Label     string    `json:"label"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type GroupInfo struct {
+	Label string `json:"label"`
+	Name  string `json:"name"`
+}
+
+type NewGroupRequest struct {
+	Label string `json:"label" validate:"required"`
+	Name  string `json:"name" validate:"required"`
+}
+
+type UpdateGroupRequest struct {
+	Label string `json:"label" validate:"required"`
+	Name  string `json:"name" validate:"required"`
 }
